e24217ac90c99465c0d190f940f79b1c75d933d9
ducnd58233 <only4wns@gmail.com>
2022-02-28 02:06:23 +0700

    resnet

--- utils/trainer.py
+++ utils/trainer.py
@@ -1,27 +1,39 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 from time import sleep
 
-def train(model, data_loader, optimizer, loss_criteria, num_classes = 1):
-    model.train()
-    train_loss = 0
+def _loops(
+    model: nn.Module,
+    data_loader: DataLoader,
+    optimizer: torch.optim,
+    loss_criteria: torch.nn,
+    num_classes: int = 1,
+    train: bool = True
+) -> float:
+
+    total_loss = 0
     correct = 0
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    device = torch.device("cuda") \
+            if torch.cuda.is_available() \
+            else torch.device("cpu")
     batch = 0
-    length = len(data_loader.dataset)   
-    
+    length = len(data_loader.dataset)
+
     with tqdm(data_loader, unit="batch") as tepoch:
         for data, target in tepoch:
-            tepoch.set_description("Train")      
+            tepoch.set_description("Train" if train else "Test")
             batch += 1
             data = data.to(device)
             target = target.to(device)
-            optimizer.zero_grad()
+            
+            if train:
+                optimizer.zero_grad()
 
             out = model(data).to(device)
             loss = loss_criteria(out, target).to(device)
-            train_loss += loss.item()
+            total_loss += loss.item()
 
             if (num_classes == 1):
                 _, predicted = torch.max(out.data, 1)
@@ -30,52 +42,73 @@ def train(model, data_loader, optimizer, loss_criteria, num_classes = 1):
                 predicted = out.argmax(dim=1)
                 correct += torch.sum(target == predicted)
 
-            loss.backward()
-            optimizer.step()
-            
+            if train:
+                loss.backward()
+                optimizer.step()
+
             acc = 100. * correct / length
             guess = f"{correct}/{length}"
-            string = f"loss: {100. * loss:.6f}%, accuracy: {acc:.6f}% [{guess}]" 
+            string = f"loss: {100. * loss:.6f}%, accuracy: {acc:.6f}% [{guess}]"
             tepoch.set_postfix_str(string)
             sleep(0.01)
-     
-    avg_loss = train_loss / (batch + 1)  
+
+    avg_loss = total_loss / (batch + 1)
     return avg_loss
-        
-        
-def test(model, data_loader, loss_criteria, num_classes = 1, type = "Test"):
+
+
+def train(
+    model: nn.Module,
+    data_loader: DataLoader,
+    optimizer: torch.optim,
+    loss_criteria: torch.nn,
+    num_classes: int = 1,
+) -> float:
+    r"""
+    Args:
+        model (nn.Module): Model use for training
+        data_loader (DataLoader): Data loader of training data
+        optimizer (torch.optim): Optimizer uses for training
+        loss_criteria (torch.nn): Loss function use for training
+        num_classes (int): Number of classes (output shape) of dataset
+    """
+
+    model.train()
+
+    return _loops(
+        model=model,
+        data_loader=data_loader,
+        optimizer=optimizer,
+        loss_criteria=loss_criteria,
+        num_classes=num_classes,
+        train=True
+    )
+
+
+def test(
+    model: nn.Module,
+    data_loader: DataLoader,
+    loss_criteria: torch.nn,
+    num_classes: int = 1,
+) -> float:
+    r"""
+    Args:
+        model (nn.Module): Model use for testing
+        data_loader (DataLoader): Data loader of testing data
+        loss_criteria (torch.nn): Loss function use for testing
+        num_classes (int): Number of classes (output shape) of dataset
+    """
+
     model.eval()
-    test_loss = 0
-    correct = 0
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    batch = 0
-    length = len(data_loader.dataset)
+    avg_loss = 0.0
     
-    with tqdm(data_loader, unit="batch") as tepoch:
-        with torch.no_grad():
-            for data, target in tepoch:
-                tepoch.set_description(type)
-                batch += 1
-                data = data.to(device)
-                target = target.to(device)
-                
-                out = model(data).to(device)
-                loss = loss_criteria(out, target).to(device)
-                test_loss += loss.item()
-                
-                if (num_classes == 1):
-                    _, predicted = torch.max(out.data, 1)
-                    correct += torch.sum(target == predicted).item()
-                else:
-                    predicted = out.argmax(dim=1)
-                    correct += torch.sum(target == predicted)
-                    
-                
-                acc = 100. * correct / length
-                guess = f"{correct}/{length}"
-                string = f"loss: {100. * loss:.6f}%, accuracy: {acc:.6f}% [{guess}]"   
-                tepoch.set_postfix_str(string)
-                sleep(0.01)
-                
-    avg_loss = test_loss / (batch + 1)           
+    with torch.no_grad():
+        avg_loss = _loops(
+            model=model,
+            data_loader=data_loader,
+            optimizer=None,
+            loss_criteria=loss_criteria,
+            num_classes=num_classes,
+            train=False
+        )
+
     return avg_loss
